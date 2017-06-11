afec0ecc82d97bd6ba94dc211a0264529417a721
root <root@localhost.localdomain>
2017-06-11 09:18:08 +0800

    poc tools

--- install.py
+++ install.py
@@ -5,6 +5,9 @@ import sys
 
 def main():
 	os.system('apt-get install figlet -y')
+	os.system('chmod o+x auto_attack.py')
+	os.system('chmod o+x pcap_review.py')
+	os.system('chmod -R o+x hotcase')
 
 if __name__ == '__main__':
 	main()
